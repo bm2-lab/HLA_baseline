ffbd9f6c7ce6646074b4da248d847a06707a6e2d
MichaelChuai <18alexanderm117@tongji.edu.cn>
2019-01-15 09:29:42 +0800

    m

--- data_proc.py
+++ data_proc.py
@@ -3,8 +3,9 @@ import tfutil
 import numpy as np
 from pyspark import SparkContext, SparkConf
 
-conf = SparkConf().setAppName('abc').setMaster('local[2]')
-sc = SparkContext(conf=conf).getOrCreate()
+n_cpu = 37
+conf = SparkConf().setAppName('abc').setMaster(f'local[{n_cpu}]')
+sc = SparkContext.getOrCreate(conf=conf)
 
 dt_aa, dt_hla = joblib.load('hla_dict.pkl')
 
@@ -17,7 +18,7 @@ def peptide_code(peptide, AA_dict, maxlen=11):
 
 
 def generator_file(file_name, dict_sample_hla_code, AA_dict):
-    sample_id = file_name.split('.')[0]
+    sample_id = file_name.split('/')[-1][:-8]
     per_hla_code = dict_sample_hla_code[sample_id]
     gn = (i.strip().split('\t') for i in open(file_name) if i.strip() != '')
     gn = map(lambda x: [x[0], int(x[1])], gn)
@@ -27,16 +28,30 @@ def generator_file(file_name, dict_sample_hla_code, AA_dict):
     return gn
 
 
-fnames = ['train_sample_0', 'train_sample_3']
-rdd_names = sc.parallelize(fnames)
+num_tr = 69
+num_te = 5
+
+dir_path = '/data/hla_raw'
+trlst = [f'full_data/train/train_sample_{i}' for i in range(num_tr)]
+telst = [f'full_data/test/test_sample_{i}' for i in range(num_te)]
+
+lst = trlst + telst
+
+save_path = '/data/hla_data'
+rdd_names = sc.parallelize(lst, numSlices=n_cpu)
 
 
 def foo(fname):
-    gn = generator_file(f'{fname}.txt', dt_hla, dt_aa)
-    tfutil.write_tfrec_from_generator(gn, fname, 2)
+    src_pos = f'{dir_path}/{fname}_pos.txt'
+    src_neg = f'{dir_path}/{fname}_neg.txt'
+    dst_pos = f'{save_path}/{fname}_pos'
+    dst_neg = f'{save_path}/{fname}_neg'
+    gn_pos = generator_file(src_pos, dt_hla, dt_aa)
+    tfutil.write_tfrec_from_generator(gn_pos, dst_pos, 2)
+    gn_neg = generator_file(src_neg, dt_hla, dt_aa)
+    tfutil.write_tfrec_from_generator(gn_neg, dst_neg, 2)
 
 
 rdd_names.foreach(foo)
 
-# gn = generator_file(f'{fname}.txt', dt_hla, dt_aa)
-# tfutil.write_tfrec_from_generator(gn, fname, 2)
+
